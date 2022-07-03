40fa2a57fc8496311bdd7c6c240189d5985d979a
ctshara14 <ctsharath14@gmail.com>
2022-07-03 08:38:49 +0530

    Adding: Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,22 @@
-//Check Employee is Present or Absent
+//Calculate daily employee wage
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		int empPresent = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == empPresent)
+		final int Present = 1;
+		final int FullTime = 8;
+		final int WagePerHour = 100;	
+		int empPresent = (int)(Math.random() * 2);
+		int empHr;
+		if (empPresent == Present) {
 			System.out.println("Employee is Present");
-		else
+			empHr = FullTime;
+		} 
+		else {
 			System.out.println("Employee is Absent");
+			empHr = 0;
 		}
+		int empWage = empHr * WagePerHour;
+		System.out.println("Employee Wage is : Rs."+empWage);
+	}
+	
 }
