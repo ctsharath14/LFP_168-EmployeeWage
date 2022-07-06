76c92d731fafefc29c6b4412aede37f6e8556a5a
ctsharath14 <102033298+ctsharath14@users.noreply.github.com>
2022-07-06 12:17:25 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,21 +2,18 @@
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		final int Present = 1;
-		final int PartTime = 2;
-		final int FullTime = 8;
-		final int WagePerHour = 100;	
+		int wagePerHour = 20;	
 		int empPresent = (int)(Math.random() * 3);
 		int empHr;
 		switch(empPresent) {
-			case Present :
+			case 1 :
 				System.out.println("Employee is Present and working full-time");
-				empHr = FullTime;	
+				empHr = 8;	
 			break;
 		
-			case PartTime :
+			case 2 :
 				System.out.println("Employee is Present and working part-time");
-				empHr = FullTime/2;
+				empHr = 4;
 			break;
 			
 			default :
@@ -25,7 +22,7 @@ public class EmployeeWage {
 			break;
 	
 		}
-		int empWage = empHr * WagePerHour;
-		System.out.println("Employee Wage is : Rs."+empWage);
+		int empWage = empHr * wagePerHour;
+		System.out.println("Employee Wage is : "+empWage+"$");
 	}
 }
