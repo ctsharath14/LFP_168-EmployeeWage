bff01b26030700217ad52ec2040373ab2c0d5874
ctsharath14 <102033298+ctsharath14@users.noreply.github.com>
2022-07-06 12:15:07 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,9 +2,8 @@
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		int empPresent = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == empPresent)
+		int empCheck = (int)(Math.random() * 2);
+		if (empCheck == 1)
 			System.out.println("Employee is Present");
 		else
 			System.out.println("Employee is Absent");
